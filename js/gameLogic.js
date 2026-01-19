36d85971a5d4904b42a374c25d2d6125a0426257
pepejoeck-codeclass <pepejoeck@gmail.com>
2026-01-18 22:14:56 -0600

    Update gameLogic.js

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,1 +1,12 @@
+import { addScore } from "./storage.js";
+
+export function initGames() {
+  const btn = document.getElementById("startGame");
+
+  btn.addEventListener("click", () => {
+    alert("Correcto 🎉 +5 puntos");
+    addScore(5);
+    location.reload();
+  });
+}
 
