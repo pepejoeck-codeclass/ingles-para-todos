aa2ece7f87ff190f6d0e3d0b980eb614f376ca5b
pepejoeck-codeclass <pepejoeck@gmail.com>
2026-01-18 22:14:00 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+import { initUI } from "./ui.js";
+import { initStorage, getScore, getLevel } from "./storage.js";
+import { initGames } from "./gameLogic.js";
+import { initI18n } from "./i18n.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initStorage();
+  initI18n();
+  initUI();
+  initGames();
+
+  updateStats();
+});
+
+function updateStats() {
+  document.getElementById("scoreText").textContent =
+    getScore() + " puntos";
+
+  document.getElementById("levelText").textContent =
+    "Nivel " + getLevel();
+}
 
